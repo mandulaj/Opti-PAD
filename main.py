c463a8e5322dd045365c57b067960be25bd27e1f
Jakub Mandula <jakub@mandula.cz>
2022-09-06 12:05:53 +0200

    fix mutation and add visualization to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ctypes
 import numpy as np
+import os
 
 from numpy.ctypeslib import ndpointer
 from tqdm import tqdm
@@ -9,27 +10,84 @@ from multiprocessing import Pool
 from tqdm.contrib.concurrent import process_map
 
 import itertools
-
+import sys
 import pandas as pd
 
-N_POPULATION = 150
+import numba
+import vis
+
+N_POPULATION = 50
 N_PIECES = 8
 PIECE_SIZES = [5, 5, 5, 5, 5, 5, 5, 6]
 # PIECE_SIZES = [4, 5, 5, 5, 5, 5, 6, 6]
 N_SCORES = 3
-N_GENERATIONS = 20
+N_GENERATIONS = 40
 
-OUT="./out"
+MUT_PROB= 0.01
+CROSS_SPLIT=1
+N_BEST=3 # N best that survive
+
+
+
+
+
+
+
+
+if len(sys.argv) == 2:
+    MUT_PROB = float(sys.argv[1])
+else:
+    MUT_PROB = 0.01
+
+OUT=f"./results/run_{MUT_PROB:.2f}_"
+
+for i in range(1000):
+    path = OUT + str(i)
+    if not os.path.exists(path):
+        os.mkdir(path)
+        OUT = path
+        break
+
+print(f"Output in {OUT}")
+
+
+
+
+
+@numba.njit
+def make_distance_matrix(genes):
+    dist_matrix = np.zeros((len(genes), len(genes)))
+
+    for i in range(len(genes)):
+        for j in range(len(genes)):
+            dist_matrix[i][j] = vis.genetic_distance(genes[i], genes[j])
+    return dist_matrix
+
+
+
+
 
 
 def fitness_function(no_sol, unqe_pieces, total_solutions):
-    no_sol_fitness = 1/((no_sol)**2+1)
-    unique_pieces_fitness = unqe_pieces/N_PIECES
+    no_sol_fitness = 1/((no_sol)**2+1) # Minimize number of no solutions
+    unique_pieces_fitness = unqe_pieces/N_PIECES # Maximize Unique Pieces
     # total_solutions_fitness = np.sqrt((total_solutions+1)/(np.max(total_solutions)+1))
-    total_solutions_fitness = 1/(total_solutions/1000+1)
+    # total_solutions_fitness = 1/(total_solutions/1000+1)
+    # total_solutions_fitness = 1-1/(total_solutions+1)
+
+    # # zero no sols
+    # total_solutions[(no_sol != 0)] = 1000
+    # minimal = np.min(total_solutions)
+
+
+
+    total_solutions_fitness = np.minimum(3200/(0.2 * total_solutions + 3000),1)
+    # total_solutions_fitness = 2/((total_solutions+1)/minimal)
+    # print(total_solutions, total_solutions_fitness)
+    # total_solutions_fitness = total_solutions/np.max(total_solutions)
     # print(no_sol_fitness, unique_pieces_fitness, total_solutions_fitness)
 
-    return no_sol_fitness * unique_pieces_fitness ** 2 * total_solutions_fitness
+    return no_sol_fitness * unique_pieces_fitness ** 3  * total_solutions_fitness
 
 
 def _process(elems):
@@ -37,6 +95,9 @@ def _process(elems):
     eval_numpy_individual(elems[0], scores_a,len(elems[0]), len(scores_a), elems[1])
     # eval_numpy_individual(gene_pool_b[i], gene_pool_scores_b[i], gene_pool_b.shape[1], gene_pool_scores_b.shape[1], restrictions)
     return scores_a
+
+
+
 
 
 def main():
@@ -52,27 +113,19 @@ def main():
     
     for gen in range(N_GENERATIONS):
         
-        # gene_pool_scores_a = np.zeros((gene_pool_a.shape[0], N_SCORES), np.uint32)
-        # gene_pool_scores_b = np.zeros((gene_pool_b.shape[0], N_SCORES), np.uint32)
+
         print(f"Generation {gen}")
         
         # Evaluate Gene pool
-        # eval_numpy_genes(gene_pool_a, gene_pool_scores_a, gene_pool_a.shape[0], gene_pool_a.shape[1], gene_pool_scores_a.shape[1], restrictions)
-        # eval_numpy_genes(gene_pool_b, gene_pool_scores_b, gene_pool_b.shape[0], gene_pool_b.shape[1], gene_pool_scores_b.shape[1], restrictions)
-
-        # process_map(_process, , max_workers=8)
-        # _process(0)
         with Pool(None) as p:
             r = tqdm(p.imap(_process, zip( gene_pool, itertools.repeat(restrictions))), total= gene_pool.shape[0])
             gene_pool_scores = np.vstack(list(r))
 
-        gene_pool_scores = gene_pool_scores.astype(np.int64)
-        no_solution_cases = gene_pool_scores[:,1]
-        total_solutions = gene_pool_scores[:,0]
+        gene_pool_scores = gene_pool_scores.astype(np.int64)    
+        no_solution_cases = gene_pool_scores[:,1]                 # Number of No-solution cases 
+        total_solutions = gene_pool_scores[:,0]                   # Total number of solutions
 
-        # fitness = (gene_pool_scores[:,2] - gene_pool_scores[:,1])**2 # Number of at least 1 solutions
-        unq_pieces = np.zeros(len(gene_pool), dtype=np.uint32)
-
+        unq_pieces = np.zeros(len(gene_pool), dtype=np.uint32)    # Unique Pieces used
         for i, gene in enumerate(gene_pool):
             unq_pieces[i] = unique_pieces(gene, len(gene))
 
@@ -80,36 +133,62 @@ def main():
         fitness = fitness_function(no_solution_cases, unq_pieces, total_solutions)
         best_fit = np.argsort(-fitness)
         probability = fitness/np.sum(fitness)
-        
+     
+     
+     
+        gene_hash = np.zeros_like(gene_pool, dtype=np.uint32)
+        genome_to_hashes(gene_pool, gene_hash, len(gene_pool), N_PIECES)
+        df_hash = pd.DataFrame(gene_hash)
+    
 
+        dist_matrix = make_distance_matrix(gene_pool)
+        # dist_matrix = np.zeros((len(genes),len(genes)))
+        dm_avg = np.average(dist_matrix)
+        dm_sum = np.sum(dist_matrix)
+        dm_median = np.median(dist_matrix)
+
+
+        ADDITIONAL_MUTATION = max(7-dm_median, 0) * 0.05
+
+
+        # Record Data
         df = pd.DataFrame(gene_pool)
+        df = pd.concat([df, df_hash], axis=1)
         df['probability'] = probability
+        df['diveristy'] = dm_avg
+        df['extra_mutation'] = ADDITIONAL_MUTATION
         df['fitness'] = fitness
         df['unique'] = unq_pieces
         df['total_sol'] = total_solutions
         df['no_sol'] = no_solution_cases
 
+
         df.sort_values("fitness", inplace=True, ascending=False)
         df.reset_index(drop=True, inplace=True)
+        df.to_csv(f"{OUT}/epoch_{gen}.csv", index=False)
+        # print_genome_color(gene_pool, len(gene_pool), N_PIECES)
 
-        df.to_csv(f"epoch_{gen}.csv", index=False)
+
+
+
+        # print_gene_pieces_horizontal(gene_pool[0], gene_hash[0],N_PIECES)
 
 
 
         # Make new pool by cross mixing fit individuals
-        new_gene_pool = makeNewGenePool(gene_pool, probability, cross=0.5)
+        new_gene_pool = makeNewGenePool(gene_pool, probability, cross=CROSS_SPLIT)
 
         # Introduce mutations
         for i in range(len(new_gene_pool)):
             for j in range(N_PIECES):
                 if j == 7:
-                    prob = [0.5, 0.5, 0]
+                    prob = MUT_PROB + ADDITIONAL_MUTATION
                 else:
-                    prob = [0.1, 0.6, 0.3]
-                new_gene_pool[i][j] = mutate(new_gene_pool[i][j], np.random.choice([4,5,6], p=prob))
+                    prob = MUT_PROB * 2 + ADDITIONAL_MUTATION
+                new_gene_pool[i][j] = mutate(new_gene_pool[i][j], prob)
 
-        # Add 3 best to gene pool without change
-        gene_pool = np.vstack((new_gene_pool, gene_pool[best_fit[0:3]]))
+        # Add N best to gene pool without change
+        gene_pool = np.vstack((new_gene_pool, gene_pool[best_fit[0:N_BEST]]))
 
         # # Print Summary of best
         # for i in range(3):
@@ -154,7 +233,13 @@ def makeNewGenePool(gene_pool, probability, cross=1):
     return new_gene_pool
 
 
-def mutate(gene, iterations, flip=0.3):
+def mutate(gene, prob=0):
+    mask = np.random.choice([0,1], size=14, replace=True, p=[1-prob,prob]) @ (1 << np.arange(14))
+    mask = mask.astype(np.uint16)
+
+    return gene ^ mask
+
+def mutate_old(gene, iterations, flip=0.3):
     mask = randomGene()
 
     while iterations:
@@ -217,6 +302,19 @@ eval_numpy_individual.argtypes = [ndpointer(ctypes.c_uint16, flags='C_CONTIGUOUS
 unique_pieces = eval.unique_pieces
 unique_pieces.argtypes = [ndpointer(ctypes.c_uint16, flags='C_CONTIGUOUS'), ctypes.c_size_t]
 unique_pieces.restype = ctypes.c_uint32
+
+
+
+print_genome_color = eval.print_genome_color
+print_genome_color.argtypes = [ndpointer(ctypes.c_uint16, flags='C_CONTIGUOUS'), ctypes.c_size_t, ctypes.c_size_t]
+
+
+genome_to_hashes = eval.genome_to_hashes
+genome_to_hashes.argtypes = [ndpointer(ctypes.c_uint16, flags='C_CONTIGUOUS'), ndpointer(ctypes.c_uint32, flags='C_CONTIGUOUS'), ctypes.c_size_t, ctypes.c_size_t]
+
+
+print_gene_pieces_horizontal = eval.print_gene_pieces_horizontal
+print_gene_pieces_horizontal.argtypes = [ndpointer(ctypes.c_uint16, flags='C_CONTIGUOUS'), ndpointer(ctypes.c_uint32, flags='C_CONTIGUOUS'), ctypes.c_size_t]
 
 
 
--- src/eval.c
+++ src/eval.c
@@ -12,9 +12,6 @@ const char *reverse_lookup_standard_local[] = {
     "4",   "3",  "2",  "1",   "",    "",    "Dec", "Nov", "Oct", "Sep", "Aug",
     "Jul", "",   "",   "Jun", "May", "Apr", "Mar", "Feb", "Jan"};
 
-board_t STANDARD_BLANK =
-    0b0000001100000011000000010000000100000001000000010001111111111111;
-
 struct game_individual {
   uint64_t n_pieces;
   // uint64_t *gene_sizes;
@@ -179,45 +176,489 @@ void print_genes(gene_t *genes, size_t n_pieces) {
   }
 }
 
-int eval(int argc, char **argv) {
+// int eval(int argc, char **argv) {
 
-  const int population_size = 100;
+//   const int population_size = 100;
 
-  uint64_t game_size = 8;
-  uint64_t game_piece_sizes[] = {5, 5, 5, 5, 5, 5, 5, 6};
+//   uint64_t game_size = 8;
+//   uint64_t game_piece_sizes[] = {5, 5, 5, 5, 5, 5, 5, 6};
 
-  struct solution_restrictions restrictions = {.use_faceup = true,
-                                               .use_facedown = false};
+//   struct solution_restrictions restrictions = {.use_faceup = true,
+//                                                .use_facedown = false};
 
-  struct game_individual *population =
-      malloc(sizeof(struct game_individual) * population_size);
-  printf("Making Gene Pool\n");
-  for (int i = 0; i < population_size; i++) {
-    make_game_individual(population + i, game_piece_sizes, game_size);
+//   struct game_individual *population =
+//       malloc(sizeof(struct game_individual) * population_size);
+//   printf("Making Gene Pool\n");
+//   for (int i = 0; i < population_size; i++) {
+//     make_game_individual(population + i, game_piece_sizes, game_size);
+//   }
+
+//   printf("Making & testing Problems\n");
+// #pragma omp parallel for schedule(dynamic)
+//   for (int i = 0; i < population_size; i++) {
+//     problem_t problem;
+//     game_individual_to_problem(&problem, &population[i], STANDARD_BLANK,
+//                                restrictions);
+//     // print_problem(&problem);
+//     // test_all_dates(&problem, restrictions);
+//   }
+
+//   printf("Done\n");
+
+//   // Clean up
+//   for (int i = 0; i < population_size; i++) {
+//     destroy_game_individual(&population[i]);
+//   }
+//   free(population);
+//   // for (int i = 0; i < population[population_size - 1].n_pieces; i++) {
+//   //   print_gene(population[population_size - 1].genes[i]);
+//   //   printf("\n");
+//   //   print_piece(gene_to_piece(population[population_size - 1].genes[i]),
+//   i);
+//   // }
+
+//   return 0;
+// }
+
+typedef uint32_t hash_t;
+
+hash_t murmur64(uint64_t h) {
+  h ^= h >> 33;
+  h *= 0xff51afd7ed558ccdL;
+  h ^= h >> 33;
+  h *= 0xc4ceb9fe1a85ec53L;
+  return h;
+}
+
+float hue2rgb(float p, float q, float t) {
+
+  if (t < 0)
+    t += 1;
+  if (t > 1)
+    t -= 1;
+  if (t < 1. / 6)
+    return p + (q - p) * 6 * t;
+  if (t < 1. / 2)
+    return q;
+  if (t < 2. / 3)
+    return p + (q - p) * (2. / 3 - t) * 6;
+
+  return p;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+/*
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns RGB in the set [0, 255].
+ */
+void hsl2rgb(float h, float s, float l, int *r, int *g, int *b) {
+
+  if (0 == s) {
+    *r = *g = *b = l; // achromatic
+  } else {
+    float q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    float p = 2 * l - q;
+    *r = hue2rgb(p, q, h + 1. / 3) * 255;
+    *g = hue2rgb(p, q, h) * 255;
+    *b = hue2rgb(p, q, h - 1. / 3) * 255;
   }
 
-  printf("Making & testing Problems\n");
-#pragma omp parallel for schedule(dynamic)
-  for (int i = 0; i < population_size; i++) {
-    problem_t problem;
-    game_individual_to_problem(&problem, &population[i], STANDARD_BLANK,
-                               restrictions);
-    // print_problem(&problem);
-    // test_all_dates(&problem, restrictions);
-  }
+  return;
+}
 
-  printf("Done\n");
-
-  // Clean up
-  for (int i = 0; i < population_size; i++) {
-    destroy_game_individual(&population[i]);
-  }
-  free(population);
-  // for (int i = 0; i < population[population_size - 1].n_pieces; i++) {
-  //   print_gene(population[population_size - 1].genes[i]);
-  //   printf("\n");
-  //   print_piece(gene_to_piece(population[population_size - 1].genes[i]), i);
-  // }
-
+/* Test main */
+#ifdef TEST_MAIN
+#include <stdio.h>
+int main(void) {
+  printf("e9c6d914c4b8d9ca == %016llx\n",
+         (unsigned long long)crc64(0, (unsigned char *)"123456789", 9));
   return 0;
 }
+#endif
+
+hash_t getSmallestHash(piece_t p) {
+  p = piece_origin(p);
+  hash_t hash = murmur64((uint64_t)p);
+  int i = 4;
+  do {
+    p = piece_origin(piece_rotate(p));
+    hash_t h = murmur64((uint64_t)p);
+    // printf("%d Smallest: %x, current: %x piece: %lx\n", i, hash, h, p);
+    if (h < hash) {
+      hash = h;
+    }
+  } while (--i);
+  return hash;
+}
+
+piece_t get_piece_smallest_hash(piece_t p) {
+  p = piece_origin(p);
+  piece_t smallest = p;
+  hash_t hash = murmur64((uint64_t)p);
+  int i = 4;
+
+  do {
+    p = piece_origin(piece_rotate(p));
+    hash_t h = murmur64((uint64_t)p);
+    if (h < hash) {
+      hash = h;
+      smallest = p;
+    }
+  } while (--i);
+  return smallest;
+}
+
+int compare_hashes(const void *a, const void *b) {
+  hash_t int_a = *((hash_t *)a);
+  hash_t int_b = *((hash_t *)b);
+
+  if (int_a == int_b)
+    return 0;
+  else if (int_a < int_b)
+    return -1;
+  else
+    return 1;
+}
+
+void genome_to_hashes(gene_t *genes, hash_t *hashes, size_t n_population,
+                      size_t n_pieces) {
+  for (int i = 0; i < n_population; i++) {
+    for (int j = 0; j < n_pieces; j++) {
+      hashes[i * n_pieces + j] =
+          getSmallestHash(gene_to_piece(genes[i * n_pieces + j]));
+    }
+    // qsort(&hashes[i * n_pieces], n_pieces, sizeof(hash_t), compare_hashes);
+  }
+}
+
+void print_genome_color(gene_t *genes, size_t n_population, size_t n_pieces) {
+  hash_t *hashes = malloc(sizeof(hash_t) * n_population * n_pieces);
+  genome_to_hashes(genes, hashes, n_population, n_pieces);
+
+  for (int i = 0; i < n_population; i++) {
+
+    hash_t *hs = &hashes[i * n_pieces];
+    int r = 0, g = 0, b = 0;
+    int r_total = 0, g_total = 0, b_total = 0;
+    for (int j = 0; j < n_pieces; j++) {
+      hsl2rgb((hs[j]) / (float)0xffffffff, 1, 0.6, &r, &g, &b);
+      r_total += r;
+      b_total += b;
+      g_total += g;
+    }
+    r = r_total / n_pieces;
+    g = g_total / n_pieces;
+    b = b_total / n_pieces;
+
+    // for (int j = 0; j < n_genes; j++) {
+    //   r += hs[j] & 0xF0;
+    //   g += (hs[j] >> 8) & 0xF0;
+    //   b += (hs[j] >> 16) & 0xF0;
+    // }
+
+    for (int j = 0; j < n_pieces; j++) {
+
+      char buffer[64];
+
+      // hsl2rgb((hs[j]) / (float)0xffffffff, 1, 0.6, &r, &g, &b);
+
+      hash_t h = hashes[i * n_pieces + j];
+      snprintf(buffer, 64, "%08x ", h);
+
+      print_rgb(buffer, r, g, b);
+      // print_color(buffer, h % 16);
+    }
+    printf("\n");
+  }
+  free(hashes);
+}
+
+void print_gene_pieces_horizontal(gene_t *genes, uint32_t *colors,
+                                  size_t n_pieces) {
+  piece_t *pieces = malloc(sizeof(piece_t) * n_pieces);
+  for (int i = 0; i < n_pieces; i++) {
+    pieces[i] = gene_to_piece(genes[i]);
+  }
+
+  char *buffer = malloc(sizeof(char) * n_pieces * 8 * 20 * n_pieces);
+  for (int i = 0; i < 8; i++) {
+    char *b = buffer;
+    for (int p = 0; p < n_pieces; p++) {
+      b += get_piece_line_rgb(pieces[p], colors[p], i, b);
+      b[0] = ' ';
+      b++;
+    }
+    printf("%s\n", buffer);
+  }
+
+  free(buffer);
+  free(pieces);
+}
+
+void print_bin(uint32_t b, size_t size) {
+  uint32_t mask = 0x01 << (size - 1);
+  for (int i = 0; i < size; i++) {
+    if (mask & b)
+      printf("1");
+    else
+      printf("0");
+
+    mask >>= 1;
+  }
+}
+
+piece_t pieces[1000];
+hash_t hashes[1000];
+size_t num = 0;
+
+int main(int argc, char **argv) {
+
+  gene_t start = 0b0100000000000000;
+  piece_t p = gene_to_piece(start);
+  hash_t h = getSmallestHash(p);
+  p = get_piece_smallest_hash(p);
+  hashes[0] = h;
+  pieces[0] = p;
+  num++;
+
+  for (gene_t g = start + 1; g < 0xFFFF; g++) {
+    p = gene_to_piece(g);
+    h = getSmallestHash(p);
+    p = get_piece_smallest_hash(p);
+
+    bool found = false;
+    for (int i = 0; i < num; i++) {
+      if (hashes[i] == h) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      hashes[num] = h;
+      pieces[num] = p;
+      num++;
+    }
+  }
+
+  printf("Num: %d\n", num);
+
+  // FILE *fp4 = fopen("4-piece.txt", "w");
+  // FILE *fp5 = fopen("5-piece.txt", "w");
+  // FILE *fp6 = fopen("6-piece.txt", "w");
+
+  printf("4 Piece\n");
+  for (int i = 0; i < 7; i++) {
+    // print_piece(pieces[i], i % 10);
+    printf("0x%lx,", pieces[i]);
+  }
+
+  printf("\n5 Piece\n");
+  for (int i = 7; i < 25; i++) {
+    // print_piece(pieces[i], i % 10);
+    printf("0x%lx,", pieces[i]);
+  }
+
+  printf("\n6 Piece\n");
+  for (int i = 25; i < 85; i++) {
+    // print_piece(pieces[i], i % 10);
+    printf("0x%lx,", pieces[i]);
+  }
+
+  return 0;
+
+  problem_t prob = {
+      .blank =
+          0b0000001100000011000000010000000100000001000000010001111111111111,
+      .reverse_lookup = reverse_lookup_standard_local,
+      .n_pieces = 8,
+      .piece_position_num = {198, 154, 196, 96, 151, 80, 48, 150},
+      .pieces = {0xe0e0000000000000, 0xe020200000000000, 0xa0e0000000000000,
+                 0xc0f0000000000000, 0x10f0000000000000, 0x40f0000000000000,
+                 0xc0c0800000000000, 0x80e0000000000000},
+      .piece_props = {{4, true},
+                      {4, true},
+                      {4, true},
+                      {4, false},
+                      {4, true},
+                      {4, false},
+                      {2, false},
+                      {4, true}}};
+
+  for (int i = 0; i < 8; i++) {
+    prob.piece_props[i] = get_piece_properties(prob.pieces[i]);
+
+    prob.piece_position_num[i] =
+        make_positions(prob.pieces[i], prob.piece_props[i], prob.blank, NULL,
+                       (struct solution_restrictions){true, true});
+    printf("%d, ", prob.piece_position_num[i]);
+  }
+
+  printf("\n");
+
+  for (int i = 0; i < 8; i++) {
+    print_piece(prob.pieces[i], i);
+  }
+
+  printf("\n");
+  for (int i = 0; i < 8; i++) {
+    printf("{%d, %s}, ", prob.piece_props[i].rotations,
+           prob.piece_props[i].asymetric ? "true" : "false");
+  }
+
+  return;
+
+  uint32_t scores[7];
+  struct solution_restrictions r = {true, true};
+
+  test_all_dates(&prob, scores, r);
+
+  printf("Total: %d No Solution: %d/%d\n", scores[0], scores[1], scores[2]);
+
+  solutions_t sol;
+
+  prob.problem = prob.blank | ((board_t)0x01 << month_location(8)) |
+                 ((board_t)0x01 << day_location(26));
+
+  init_solutions(&sol, &prob, r);
+
+  for (int i = 0; i < 8; i++) {
+    printf("Sol id: %d\n", sol.sorted_sol_indexes[i]);
+  }
+
+  return 0;
+  solve(&sol);
+
+  printf("Found %ld solutions:\n", sol.num_solutions);
+
+  for (int i = 0; i < sol.num_solutions; i++) {
+    print_solution(&sol.solutions[i], &prob);
+  }
+
+  destroy_solutions(&sol);
+
+  int N = 0x1 << (2 + 3);
+  int n_shapes = 0;
+  piece_t shapes[N];
+  int shape_counts[N];
+  for (int i = 0; i < N; i++) {
+    shape_counts[i] = 0;
+  }
+
+  // gene_t g = 0x81f7;
+  // print_piece(gene_to_piece(g), 0);
+  // printf("%x\n", getSmallestHash(gene_to_piece(g)));
+  // print_piece(get_piece_smallest_hash(gene_to_piece(g)), 0);
+
+  // g = 0x81fe;
+  // print_piece(gene_to_piece(g), 1);
+  // printf("%x\n", getSmallestHash(gene_to_piece(g)));
+  // print_piece(get_piece_smallest_hash(gene_to_piece(g)), 1);
+
+  // return;
+  // // for (gene_t g = 0; g < 32; g++) {
+  //   gene_t gg = g | 0x4000;
+  //   printf("%04x\n", gg);
+  //   print_bin(gg >> 5, 4);
+  //   printf(" ");
+  //   print_bin(gg >> 2, 3);
+  //   printf(" ");
+  //   print_bin(gg, 2);
+  //   printf("\n");
+  //   print_piece(get_piece_smallest_hash(gene_to_piece(gg)), g % 10);
+  // }
+
+  for (gene_t g = 0; g < N; g++) {
+    gene_t gg = g | 0x4000;
+    // gene_t gg = g | 0xC000;
+    // gene_t gg = g | 0x8000;
+    // printf("%04x\n", gg);
+    // print_bin(gg >> 5, 4);
+    // printf(" ");
+    // print_bin(gg >> 2, 3);
+    // printf(" ");
+    // print_bin(gg, 2);
+    // printf("\n");
+    // print_piece(get_piece_smallest_hash(gene_to_piece(gg)), g % 10);
+    piece_t smallest = get_piece_smallest_hash(gene_to_piece(gg));
+    int index = 0;
+    for (int i = 0; i < n_shapes; i++) {
+      if (smallest == shapes[i])
+        break;
+      index++;
+    }
+    if (index >= n_shapes) {
+      shapes[index] = smallest;
+      n_shapes++;
+    }
+
+    shape_counts[index]++;
+  }
+
+  int total = 0;
+  for (int i = 0; i < n_shapes; i++) {
+    printf("%d\n", shape_counts[i]);
+    print_piece(shapes[i], i % 10);
+    total += shape_counts[i];
+  }
+  printf("N-shapes: %d, Total: %d\n", n_shapes, total);
+
+  return 0;
+  const int pop = 50;
+  const int n_genes = 8;
+  gene_t genes[pop * n_genes];
+
+  for (int i = 0; i < pop * n_genes; i++) {
+    genes[i] = gene_random(i % 3 + 4);
+    piece_t p = gene_to_piece(genes[i]);
+    hash_t h = getSmallestHash(p);
+    printf("%x\n", h);
+    print_piece(p, h);
+  }
+
+  hash_t hashes[pop * n_genes];
+  genome_to_hashes(genes, hashes, pop, n_genes);
+
+  for (int i = 0; i < pop; i++) {
+
+    hash_t *hs = &hashes[i * n_genes];
+    int r = 0, g = 0, b = 0;
+    // for (int j = 0; j < n_genes; j++) {
+    //   r += hs[j] & 0xF0;
+    //   g += (hs[j] >> 8) & 0xF0;
+    //   b += (hs[j] >> 16) & 0xF0;
+    // }
+
+    for (int j = 0; j < n_genes; j++) {
+
+      char buffer[64];
+
+      hsl2rgb((hs[j]) / (float)0xffffffff, 1, 0.6, &r, &g, &b);
+
+      hash_t h = hashes[i * n_genes + j];
+      snprintf(buffer, 64, "%08x ", h);
+
+      print_rgb(buffer, r, g, b);
+      // print_color(buffer, h % 16);
+    }
+    printf("\n");
+  }
+
+  // for (int i = 0; i < sizeof(pieces) / sizeof(pieces[0]); i++) {
+  //   piece_t p = pieces[i];
+  //   for (int j = 0; j < 4; j++) {
+  //     p = piece_sft_down(piece_rotate(p));
+  //     print_piece(p, i % 10);
+
+  //     printf("HASH: %lx\n", getSmallestHash(p));
+  //   }
+  // }
+  // for (int i = 0; i < sizeof(pieces) / sizeof(pieces[0]); i++) {
+  //   piece_t p = pieces[i];
+  // }
+}
+
+void getPieceHashList() {}
